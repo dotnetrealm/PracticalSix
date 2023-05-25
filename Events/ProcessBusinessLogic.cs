1f1f284bbbdf2d1759b8ae13669ebac6116228ba
Bhavin Kareliya <bhavin.k@simformsolutions.com>
2023-05-25 10:48:29 +0530

    Add: Events using delegates

---
+++ Events/ProcessBusinessLogic.cs
@@ -0,0 +1,19 @@
+﻿namespace Events
+{
+    public delegate void PostEvents();
+    public class ProcessBusinessLogic
+    {
+        public PostEvents ProcessCompleted;   
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+            Thread.Sleep(1500);
+            OnProcessCompleted();
+        }
+
+        protected virtual void OnProcessCompleted()
+        {
+            ProcessCompleted?.Invoke();
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,15 @@
+﻿using Events;
+
+Console.WriteLine("Task: Events");
+Console.WriteLine("============");
+
+ProcessBusinessLogic process = new();
+process.ProcessCompleted += bl_ProcessCompleted;
+
+process.StartProcess();
+
+static void bl_ProcessCompleted() {
+    Console.WriteLine("Method Invoked");
+}
+
+Console.ReadLine();
